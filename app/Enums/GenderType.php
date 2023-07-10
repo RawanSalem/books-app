2bcfd6d5221e5ef317b1c58804b8e3cdb1717651
Rawan <rawan.aldemaeen@ibtikar.net.sa>
2023-07-10 23:56:03 +0300

    create reader model, update auther and user models

---
+++ app/Enums/GenderType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+use BenSampo\Enum\Enum;
+
+final class GenderType extends Enum {
+
+    const NOT_SPECIFIED = 0;
+    const FEMALE = 1;
+
+    const MALE = 2;
+}
--- database/migrations/2023_07_10_202430_create_readers_table.php
+++ database/migrations/2023_07_10_202430_create_readers_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAuthorsTable extends Migration
+class CreateReadersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class CreateAuthorsTable extends Migration
      */
     public function up()
     {
-        Schema::create('authors', function (Blueprint $table) {
+        Schema::create('readers', function (Blueprint $table) {
             $table->id()->autoIncrement();
             $table->foreignId('user_id')->constrained('users')->nullable();
             $table->string('name');
@@ -35,6 +35,6 @@ class CreateAuthorsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('authors');
+        Schema::dropIfExists('readers');
     }
 }
